120a4443ef06b4344e83f6db0299f95b9ed06d9a
Alexander <renyreny125tt@gmail.com>
2022-03-12 20:49:11 +0300

    docs: refactor start

--- src/orm_config.ts
+++ src/orm_config.ts
@@ -2,11 +2,12 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-34-247-151-118.eu-west-1.compute.amazonaws.com',
+  url: 'postgres://rwsgndpgmlpckv:10a0795ba77b58ef34755956aa379fb33bb441f2e6db36f2e63c637c7c3c6dec@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d3hcg5j8b3hhmf',
   port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'postgres',
+  username: 'rwsgndpgmlpckv',
+  password: '10a0795ba77b58ef34755956aa379fb33bb441f2e6db36f2e63c637c7c3c6dec',
+  database: 'd3hcg5j8b3hhmf',
   dropSchema: true,
   logging: true,
   synchronize: false,
